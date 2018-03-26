586b86e40945cb9c6c7bb26d926f2139aaf6273e
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-03-25 19:44:24 -0700

    add authentication flow using Netlify Login Widget

---
+++ src/App/functions/handleAuthentication.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
+
+const handleAuthentication = (that) => {
+	netlifyIdentity.on('login', ({ id, user_metadata: { full_name: name }, token: { access_token: token } }) => {
+		that.setState({ user: { id, name, token } })
+	})
+	netlifyIdentity.on('logout', () => {
+		that.setState({ user: null })
+	})
+	netlifyIdentity.on('error', (error) => {
+		that.setState({ user: null })
+		console.log('Error on handleAuthentication', error)
+	})
+	window.netlifyIdentity = netlifyIdentity
+	netlifyIdentity.init()
+}
+
+export default handleAuthentication
--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import handleAuthentication from './functions/handleAuthentication'
 import { navbar, login, notFound, footer } from './routes'
 import ErrorBoundary from './components/ErrorBoundary/index'
 import { main, content } from './styles'
 
 export default class App extends Component {
+	state = { user: null }
+	componentDidMount = () => { handleAuthentication(this) }
 	render = () => (
 		<div style={main}>
 			<ErrorBoundary>
